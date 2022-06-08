14666f77faa40dbd825ab4bc3076f52df3d2da1a
Emmanuel O <emmanuelo@phreetech.com>
2022-06-08 01:04:28 +0100

    update

--- components/OTP_popup.tsx
+++ components/OTP_popup.tsx
@@ -6,9 +6,13 @@ interface Props { }
 let currentOTPIndex: number = 0
 
 const OTPField = ({ }: Props) => {
+    const [isLocked, setIsLocked] = useState(false)
+    const [animate, setAnimate] = useState(false)
     const [otp, setOtp] = useState<string[]>(new Array(6).fill(""));
     const [activeOTPIndex, setActiveOTPIndex] = useState<number>(0);
     const inputRef = useRef<HTMLInputElement>(null)
+
+
     const handleOnChange = (
         { target }: React.ChangeEvent<HTMLInputElement>,
     ): void => {
@@ -34,23 +38,45 @@ const OTPField = ({ }: Props) => {
         inputRef.current?.focus();
     }, [activeOTPIndex])
 
-    const verifyOTP = () => {
-        console.log(otp)
+
+
+    const emptyCount = otp.filter(val => val.length === 0).length;
+    function otp_length() {
+        if (emptyCount === 0) return 'Verify OTP'
+        if (emptyCount === 1) return emptyCount + " " + `Digit left`
+        if (emptyCount !== 0) return emptyCount + " " + `Digits left`
     }
+
+
+
+    const verifyOTP = (value: any) => {
+        value.preventDefault();
+
+        const allEqual = otp.every(v => v === otp[0])
+        if (allEqual === true) return setIsLocked(!isLocked)
+        setAnimate(!animate)
+        setTimeout(() => {
+            setAnimate(!animate)
+        },0.001)
+    }
+
 
 
     return (
         <div className="otp-wrapper">
-            <div>
-                <h1>Please enter OTP to verify account</h1>
-                <h1>OTP has been sent to your email</h1>
-            </div>
-            <form onSubmit={verifyOTP}>
-                <div className="otp-form">
-                    {otp.map((_, index) => {
-                        return (
-                            <div>
-                                <React.Fragment key={index}>
+            <div className="otp-container">
+                <div className={`container-lock ${!isLocked ? 'otp-null' : 'unlocked otp-success'}`}>
+                    <span className={`lock ${!isLocked ? (animate && 'lock-shake') : 'unlocked'}`}></span>
+                </div>
+                <div>
+                    <h1 className="text-lg truncate">Please enter OTP to verify your account</h1>
+                    <h2 className="text-sm opacity-70 pb-6">OTP has been sent to your email</h2>
+                </div>
+                <form onSubmit={(e) => verifyOTP(e)}>
+                    <div className="otp-form space-x-4">
+                        {otp.map((_, index) => {
+                            return (
+                                <div key={index}>
                                     <input
                                         ref={index === activeOTPIndex ? inputRef : null}
                                         type="number"
@@ -59,13 +85,17 @@ const OTPField = ({ }: Props) => {
                                         value={otp[index]}
                                         onKeyDown={(e) => handleOnKeyDown(e, index)}
                                     />
-                                </React.Fragment>
-                            </div>
-                        );
-                    })}
-                </div>
-                <button type="submit">Verify Otp</button>
-            </form>
+                                </div>
+                            );
+                        })}
+                    </div>
+                    <div className="resend-otp">
+                        <p>Didn't recieve OTP?</p>
+                        <div> Resend OTP </div>
+                    </div>
+                    <button disabled={emptyCount === 0 ? false : true} className={`otp-button ${emptyCount === 0 ? 'otp-button-ok' : 'otp-button-disabled'}`} type="submit">{otp_length()}</button>
+                </form>
+            </div>
         </div>
     );
 };
