471eaf71567b3d93b55313d5c7e431c9fcbd3d9c
Emmanuel Onyeyaforo <rahzy24@gmail.com>
2022-07-11 19:29:34 +0100

    update

--- features/auth/authService.js
+++ features/auth/authService.js
@@ -11,21 +11,21 @@ const register = async (userData) => {
   return response.data.message
 }
 
-// // Login user
-// const login = async (userData) => {
-//   const response = await axios.post(API_URL + 'login', userData)
+// Login user
+const login = async (userData) => {
+  const response = await axios.post(API_URL + 'login', userData)
 
-//   if (response.data) {
-//     localStorage.setItem('user', JSON.stringify(response.data))
-//   }
+  if (response.data) {
+    localStorage.setItem('user', JSON.stringify(response.data))
+  }
 
-//   return response.data
-// }
+  return response.data
+}
 
-// // Logout user
-// const logout = () => {
-//   localStorage.removeItem('user')
-// }
+// Logout user
+const logout = () => {
+  localStorage.removeItem('user')
+}
 
 const authService = {
   register,
