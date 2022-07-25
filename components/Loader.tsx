9770d4ac2e1fe3b492013961d158fd6e9b2dd6d4
Emmanuel Onyeyaforo <rahzy24@gmail.com>
2022-07-25 12:09:28 +0100

    styles and templates

---
+++ components/Loader.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loader = () => {
+    return (
+        <div className="progress-1" >
+            <div className="indeterminate" />
+        </div >
+    )
+}
+
+export default Loader
